cf958c85ff31d5d40880c3798b7a5d4f23ee38cb
Rafael <rafa_rodrigues01@yahoo.com>
2023-11-11 16:12:46 -0300

    atualizado conexão usando o .env

--- src/conexao.js
+++ src/conexao.js
@@ -1,29 +1,11 @@
-// const { Pool } = require('pg');
-
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'market_cubos',
-//   password: '010597',
-//   port: 5432,
-// });
-
-// const query = (text, param) => {
-//   return pool.query(text, param);
-// };
-
-// module.exports = {
-//   query,
-// };
-
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: '010597',
-    database: 'market_cubos',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_USER,
   },
 });
 
